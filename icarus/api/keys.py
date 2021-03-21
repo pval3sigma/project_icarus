8a1dc826e68fc5ce67978389a256d5160e40198c
cemkarabulut <48982235+cemkarabulut@users.noreply.github.com>
2021-03-21 22:19:17 +0100

    Delete icarus directory

--- icarus/api/keys.py
+++
@@ -1,7 +0,0 @@
-
-def get_keys():
-    BinanceKey = dict()
-    BinanceKey['api_key'] = input('Enter api key: ')
-    BinanceKey['api_secret'] = input('Enter api secret key : ')
-
-    return BinanceKey
